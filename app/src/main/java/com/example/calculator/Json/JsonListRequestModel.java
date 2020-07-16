1758b5f27299be57e465e8674ce9d44a69cd4b7d
Romanos <romanos.panagakos@gmail.com>
2020-07-16 11:25:14 +0300

    Update

--- app/src/main/java/com/example/calculator/Json/JsonListRequestModel.java
+++ app/src/main/java/com/example/calculator/Json/JsonListRequestModel.java
@@ -19,25 +19,6 @@ public class JsonListRequestModel {
         this.rates = rates;
     }
 
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public void setTimestamp(int timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public void setBase(String base) {
-        this.base = base;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public void setRates(RatesJsonModel rates) {
-        this.rates = rates;
-    }
 
     public boolean getSuccess() {
         return success;
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -12,16 +17,24 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.calculator.Json.JsonListRequestModel;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String baseCurrency = "EUR";
-    private String convertedCurrency = "USD";
-    private float convRate = 0f;
+    private Spinner first, second;
+    private  HashMap<String, Double> list = new HashMap<>();
+    List<String> listOfKeys = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,24 +45,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
+        first = findViewById(R.id.firstConversion);
+        second = findViewById(R.id.secondConversion);
+        requestData(first);
+        Log.d("RELIST2222", listOfKeys.toString());
 
 
     }
 
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-
+    private void requestData(final Spinner spinner){
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "http://data.fixer.io/api/latest?access_key=0b4ab42fb72c68017105b6e43e650700&format=1";
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONObject result = jsonObj.getJSONObject("rates");
+                            list = new Gson().fromJson(String.valueOf(result), new TypeToken<HashMap<String, Double>>() {}.getType());
+                            Log.i("MAP", "SomeText: " + new Gson().toJson(list));
 
-                        JsonListRequestModel model = new Gson().fromJson(response, JsonListRequestModel.class);
-                        Log.i("RESPONSE", model.toString());
+                            listOfKeys = new ArrayList<String>(list.keySet());
+                            Log.d("RELIST", listOfKeys.toString());
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, listOfKeys);
+                            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            spinner.setAdapter(adapter);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            //Log.i("MAP", e.toString());
+
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
